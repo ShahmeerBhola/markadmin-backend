656064ba04316c20bbb9f7bd6f5ac2df82ca9002
ShahmeerBhola <shahmir.shamim1@gmail.com>
2022-09-26 22:59:30 +0500

    push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express')
 const cors=require('cors')
 const bodyParser=require('body-parser');
 const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
 const app=express();
 require('dotenv').config();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -10,5 +11,5 @@ app.use(cors());
 connectDB();
 var routes=require('./routes/routes');
 routes(app)
-app.listen(5000,()=>console.log('Node running at 5000'))
+app.listen(port,()=>console.log('Node running at 5000'))
 app.use(cors());
